7c145d20c7af2a7b1f8169c999dbfa4b87ee165d
Leo Antunes <antunesleo4@gmail.com>
2024-01-30 19:51:54 -0300

    add volumes do api service and create places route

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+from src.places import router as places_router
+
 
 app = FastAPI()
-
+app.include_router(places_router.router)
 
 @app.get("/ping")
 def read_ping():
